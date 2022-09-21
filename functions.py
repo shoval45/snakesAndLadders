416c82f62f5d5fc1f6eeebc2b3d7ce12415c901b
shiraisha <shirnoa3@gmail.com>
2022-09-21 15:47:25 +0300

    32222232333333333

--- functions.py
+++ functions.py
@@ -1,9 +1,36 @@
 import random
-import dice
+import tkinter
+import consts
 
-path = ['dice1.jpg', 'dice2.jpg', 'dice3.jpg', 'dice4.jpg', 'dice5.jpg', 'dice6.jpg']
 
-rnd_dice = random.randint(0,5)
-photo_dice = dice.imread(path[rnd_dice])
-dice.imshow(str(rnd_dice + 1), photo_dice)
-dice.waitKey(0)
+def random_dice():
+    FONT_SIZE = 40
+
+    root = tkinter.Tk()
+    root.geometry("300x200")
+    root.title("DICE")
+
+    rnd_number = random.randint(1,6)
+    if rnd_number == 1:
+        dice_label = tkinter.Label(root, text='1', font=(consts.FONT_NAME, FONT_SIZE))
+        dice_label.place(x=150, y=70)
+    if rnd_number == 2:
+        dice_label = tkinter.Label(root, text='2', font=(consts.FONT_NAME, FONT_SIZE))
+        dice_label.place(x=150, y=70)
+    if rnd_number == 3:
+        dice_label = tkinter.Label(root, text='3', font=(consts.FONT_NAME, FONT_SIZE))
+        dice_label.place(x=150, y=70)
+    if rnd_number == 4:
+        dice_label = tkinter.Label(root, text='4', font=(consts.FONT_NAME, FONT_SIZE))
+        dice_label.place(x=150, y=70)
+    if rnd_number == 5:
+        dice_label = tkinter.Label(root, text='5', font=(consts.FONT_NAME, FONT_SIZE))
+        dice_label.place(x=150, y=70)
+    if rnd_number == 6:
+        dice_label = tkinter.Label(root, text='6', font=(consts.FONT_NAME, FONT_SIZE))
+        dice_label.place(x=150, y=70)
+    root.mainloop()
+    return rnd_number
+
+
+random_dice()
