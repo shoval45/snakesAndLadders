b81251a001f86198216ed2aba1e7523231153866
shoval45 <shoval29102004@gmail.com>
2022-09-21 16:02:48 +0300

    hh

--- Screen.py
+++ Screen.py
@@ -38,33 +38,33 @@ def draw_message(message, font_size, color, location):
     text_img = font.render(message, True, color)
     WIN.blit(text_img, location)
 
-# def draw_game(game_state):
-#     draw_board()
-#     if game_state["pressed_key_enter"]:
-#         draw_grid()
-#         time.sleep(1)
+def draw_game(game_state):
+    draw_board()
+    if game_state["pressed_key_enter"]:
+        draw_grid()
+        time.sleep(1)
+
+    elif game_state["state"] == consts.LOSE_STATE:
+        draw_lose_message()
+        time.sleep(3)
+
+    elif game_state["state"] == consts.WIN_STATE:
+        draw_win_message()
+        time.sleep(3)
+
+    pygame.display.flip()
+
+
+
+# def main():
+#     run = True
+#     while run:
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 run = False
+#         draw_board()
 #
-#     elif game_state["state"] == consts.LOSE_STATE:
-#         draw_lose_message()
-#         time.sleep(3)
+#     pygame.quit()
 #
-#     elif game_state["state"] == consts.WIN_STATE:
-#         draw_win_message()
-#         time.sleep(3)
-#
-#     pygame.display.flip()
-
-
-
-def main():
-    run = True
-    while run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-        draw_board()
-
-    pygame.quit()
-
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import pygame
+
+import Screen
+import board_grid
 import consts
+import player
 
 state = {
     "is_window_open": True,
@@ -7,3 +12,59 @@ state = {
     "player_location": [6, 0]
 }
 
+grid = board_grid.returned_initialized_grid()
+
+def main():
+    pygame.init()
+    main_player = player.player()
+
+    while state["is_window_open"]:
+        handle_user_events(main_player)
+
+        if is_lose():
+            state["state"] = consts.LOSE_STATE
+            state["is_window_open"] = False
+
+        elif is_win():
+            state["state"] = consts.WIN_STATE
+            state["is_window_open"] = False
+
+        Screen.draw_game(state)
+
+
+def handle_user_events(main_player):
+    keys_pressed = pygame.key.get_pressed()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            state["is_window_open"] = False
+
+        elif state["state"] != consts.RUNNING_STATE:
+            continue
+
+        if keys_pressed[pygame.K_RETURN]:
+            main_player.x
+
+        # need randomized and present question if question not presented in 3 times
+
+
+
+
+def compare_lists_locations(list1, list2):
+    if list1[0] == list2[0]:
+        if list1[1] == list2[2]:
+            return True
+    return False
+
+def is_lose():
+    if state["num of mistakes"] >= 3:
+        return True
+    return False
+
+def is_win():
+    win_location_in_grid = [0, 6]
+    if compare_lists_locations(state["player_location"], win_location_in_grid):
+        return True
+    return False
+
+if __name__ == '__main__':
+    main()
